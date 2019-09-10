d229328a66383b6b4a026564e09698958d8c273f
jfkquality <jfkquality@gmail.com>
2019-09-10 13:33:15 +0000

    0x00 Python #8. Updated. Works but too manylines.

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+str = str[str.find("object-oriented programming"):
+          str.find("object-oriented programming") +
+          len("object-oriented programming")]\
+ + str[str.find(" with "):str.find(" with ") + len(" with ")]\
+ + str[str.find("Python"):str.find("Python") + len("Python")]
 print(str)
