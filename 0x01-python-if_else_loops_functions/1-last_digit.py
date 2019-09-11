332901b7ca9e0b70853b7da5f7ba64cd03af3d70
jfkquality <jfkquality@gmail.com>
2019-09-11 17:08:00 +0000

    Debug 0x01 #1. Make all numbers abs(number); fix test for > 5 from < 5.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastdigit = (number % 10)
-print(lastdigit)
+makepos = abs(number)
+lastdigit = (makepos % 10)
+
 print('Last digit of {:d} is {:d}'.format(number, lastdigit), end='')
-if lastdigit < 5:
+if lastdigit > 5:
     print(' and is greater than 5')
 elif lastdigit == 0:
     print(' and is 0')
