ed2b579ee5149abd6dc34662611a5de6d66aaade
jfkquality <jfkquality@gmail.com>
2019-09-11 02:53:30 +0000

    0x01 Python #6. Debug to pass linter.

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# If the reverse of a number is less than the number, it's a dup; don't print it.
+# If the reverse of a num is less than the num, it's a dup; don't print it.
 # Reverse a number buy mod 10 times 10 plus quotient of divide by 10
 
 for i in range(0, 89):
@@ -10,4 +10,3 @@ for i in range(0, 89):
 for i in range(80, 99):
     if i < (i % 10 * 10) + (i // 10) and i != (i % 10 * 10) + (i // 10):
         print("{:02d}".format(i))
-
