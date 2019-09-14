7fcda9f45b23cf4d32c2ad5c0f156e3fece77d12
jfkquality <jfkquality@gmail.com>
2019-09-14 01:46:17 +0000

    0x02 #2, 3, 5. Put main execute guard in main executable.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 import sys
+if __name__ == "__main__":
+    argslen = len(sys.argv)
+    if argslen == 1:
+        print("0 arguments.")
+    elif argslen == 2:
+        print("1 argument:")
+    else:
+        print("{:d} arguments:".format(argslen-1))
 
-argslen = len(sys.argv)
-if argslen == 1:
-    print("0 arguments.")
-elif argslen == 2:
-    print("1 argument:")
-else:
-    print("{:d} arguments:".format(argslen-1))
-
-if argslen > 1:
-    for i, arg in enumerate(sys.argv):
-        if i == 0:
-            continue
-        print("{:d}: {}".format(i, arg))
+        if argslen > 1:
+            for i, arg in enumerate(sys.argv):
+                if i == 0:
+                    continue
+                print("{:d}: {}".format(i, arg))
