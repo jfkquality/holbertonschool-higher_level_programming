a745cce987b9c0b6da5373b5b6293429d304407c
jfkquality <jfkquality@gmail.com>
2019-09-17 19:42:00 +0000

    .

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
-
-
 def print_list_integer(my_list=[]):
-    """My print list function
-
-    Args:
-        my_list: the list
-
-    Returns:
-        Nothing.
-    """
-
     for item in range(len(my_list)):
         print('{}'.format(my_list[item]))
