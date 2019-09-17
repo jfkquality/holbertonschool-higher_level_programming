4c1453209c6278bf1c961c6c426011d96c1c8123
jfkquality <jfkquality@gmail.com>
2019-09-17 17:51:34 +0000

    0x03 Python #0. Init commit.

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+
+    """My print list function
+
+    Args:
+        my_list: the list
+
+    Returns:
+        Nothing.
+    """
+
+    for item in my_list:
+        print('{}'.format(my_list[item - 1]))
