395d40f6a06c85164528e6e6d1820b6a5a4d1de7
jfkquality <jfkquality@gmail.com>
2019-09-18 20:53:29 +0000

    0x03. #6. Try different solution with join().

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for val in row:
-            print('{:d}'.format(val), end=' '
-                  if row.index(val) < len(row) - 1 else '')
-        print()
+    print('\n'.join([' '.join(['{:d}'.format(item) for item in row])
+        for row in matrix]))
+
+    # for row in matrix:
+    #     for val in row:
+    #         print('{:d}'.format(val), end=' '
+    #               if row.index(val) < len(row) - 1 else '')
+    #     print()
--- 0x03-python-data_structures/6-print_matrix_integer_all3.py
+++ 0x03-python-data_structures/6-print_matrix_integer_all3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    # print('\n'.join([''.join(['{:2d}'.format(item) for item in row])
-    #                  for row in matrix]))
+    print('\n'.join([' '.join(['{:d}'.format(item) for item in row])
+        for row in matrix]))
+          # , end = ' ' if row.index(item) < len(row) - 1 else '')
 
     # for i in range(len(matrix)):
     #     for j in range(len(matrix[i])):
@@ -9,8 +10,8 @@ def print_matrix_integer(matrix=[[]]):
     #               if j < len(matrix[i]) - 1 else '')
     #     print()
 
-    for row in matrix:
-        for val in row:
-            print ('{:d}'.format(val), end=' '
-                   if row.index(val) < len(row) - 1 else '')
-        print()
+    # for row in matrix:
+    #     for val in row:
+    #         print ('{:d}'.format(val), end=' '
+    #                if row.index(val) < len(row) - 1 else '')
+    #     print()
