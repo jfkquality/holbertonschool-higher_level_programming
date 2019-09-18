668ebbf22c06c38576ddae93eef03e7e0183d9ef
jfkquality <jfkquality@gmail.com>
2019-09-18 20:40:52 +0000

    0x03. Debug #6. Linter issue.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    # for row in matrix:
-    #     for val in row:
-    #         print ('{:d}'.format(val), end='')
-    #     print()
     for row in matrix:
         for val in row:
-            print ('{:d}'.format(val), end=' '
-                if row.index(val) < len(row) - 1 else '')
+            print('{:d}'.format(val), end=' '
+                  if row.index(val) < len(row) - 1 else '')
         print()
