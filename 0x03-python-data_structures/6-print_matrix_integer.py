c950a94b14134fb1ba2b5ce6b72763f6b22f944c
jfkquality <jfkquality@gmail.com>
2019-09-18 01:38:15 +0000

    0x03 #6. Init commit. THREE ways to solve it!

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    # print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in matrix]))
+
+    # for i in range(len(matrix)):
+    #     for j in range(len(matrix[i])):
+    #         print('{:4}'.format(matrix[i][j]), end=''),
+    #     print()
+
+    for row in matrix:
+        for val in row:
+            print ('{:4}'.format(val), end='')
+        print()
