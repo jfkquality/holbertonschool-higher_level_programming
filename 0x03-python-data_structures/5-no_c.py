4c4d6f2962a35179c8059a3560a05d3892ca3532
jfkquality <jfkquality@gmail.com>
2019-09-18 01:11:00 +0000

    Debug 0x03 #5. Found str.translate() method!

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    newstring = []
-    for char in range(len(my_string)):
-        # print(newstring)
-        if my_string[char] == "c" or my_string[char] == "C":
-            # print (len(newstring), char)
-            newstring = my_string[len(newstring):char] + my_string[char + 1:]
-    # print(newstring)
-    return (newstring)
+    my_string = my_string.translate({ord(i): None for i in 'cC'})
+    return (my_string)
