ab2365773155d88f7b3a292e062cba696bc0e728
jfkquality <jfkquality@gmail.com>
2019-09-18 03:50:20 +0000

    0x03 #11. Linter error.

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -6,5 +6,4 @@ def delete_at(my_list=[], idx=0):
     for i in range(len(my_list)):
         if (i == idx):
             my_list.remove(my_list[i])
-            # my_list = my_list.translate({ord(my_list[i]): None})
     return (my_list)
