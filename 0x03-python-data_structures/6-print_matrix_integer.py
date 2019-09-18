da07b2bf7b761723c6a41eedcacf4dfa76a9f4bc
jfkquality <jfkquality@gmail.com>
2019-09-18 01:44:03 +0000

    0x03 #6. Linter errors.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    # print('\n'.join([''.join(['{:2}'.format(item) for item in row]) for row in matrix]))
+    print('\n'.join([''.join(['{:2}'.format(item) for item in row])
+                     for row in matrix]))
 
     # for i in range(len(matrix)):
     #     for j in range(len(matrix[i])):
     #         print('{:2}'.format(matrix[i][j]), end=''),
     #     print()
 
-    for row in matrix:
-        for val in row:
-            print ('{:2}'.format(val), end='')
-        print()
+    # for row in matrix:
+    #     for val in row:
+    #         print ('{:2}'.format(val), end='')
+    #     print()
