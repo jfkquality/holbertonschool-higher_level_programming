7a551d40324a235c93aec28db522e78ce12adcaa
jfkquality <jfkquality@gmail.com>
2019-09-18 01:40:33 +0000

    0x03 #6. Reduce output from length 4 to 2.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    # print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in matrix]))
+    # print('\n'.join([''.join(['{:2}'.format(item) for item in row]) for row in matrix]))
 
     # for i in range(len(matrix)):
     #     for j in range(len(matrix[i])):
-    #         print('{:4}'.format(matrix[i][j]), end=''),
+    #         print('{:2}'.format(matrix[i][j]), end=''),
     #     print()
 
     for row in matrix:
         for val in row:
-            print ('{:4}'.format(val), end='')
+            print ('{:2}'.format(val), end='')
         print()
