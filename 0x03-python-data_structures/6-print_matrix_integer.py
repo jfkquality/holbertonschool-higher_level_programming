505f5dde2c9696ed11d33c5405a34cd31d07b028
jfkquality <jfkquality@gmail.com>
2019-09-18 20:36:33 +0000

    0x03. Debug #6. Correct printing spaces.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    # for row in matrix:
+    #     for val in row:
+    #         print ('{:d}'.format(val), end='')
+    #     print()
     for row in matrix:
         for val in row:
-            print ('{:2d}'.format(val), end='')
+            print ('{:d}'.format(val), end=' '
+                if row.index(val) < len(row) - 1 else '')
         print()
