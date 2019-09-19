9795c9a1464a818f917f011d76c94a4e1aed1a6c
jfkquality <jfkquality@gmail.com>
2019-09-19 20:44:11 +0000

    0x04. #0 init commit.

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    # if __name__ == "__main__":
+        newmatrix = list(map(list, matrix))
+        for i, row in enumerate(matrix):
+            for j, col in enumerate(row):
+                newmatrix[i][j] = (matrix[i][j] * matrix[i][j])
+        return (newmatrix)
