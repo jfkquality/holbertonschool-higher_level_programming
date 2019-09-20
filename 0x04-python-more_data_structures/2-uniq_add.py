667b31a848cb7fcc571241d1fb2f6a71b9db5fd9
jfkquality <jfkquality@gmail.com>
2019-09-20 01:58:53 +0000

    0x04. #2 init commit.

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    newlist = list(map(list, my_list))
-    for i, val in enumerate(newlist):
-            if val == search:
-                newlist[i] = replace
+    sumlist = []
+    sum = 0
+    for i, val1 in enumerate(my_list):
+        if val1 not in sumlist:
+            sumlist.append(val1)
+    for val2 in sumlist:
+        sum += int(val2)
+    return sum
