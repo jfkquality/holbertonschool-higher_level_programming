824a6107503b2163400a2553130bc66b4f604508
jfkquality <jfkquality@gmail.com>
2019-09-20 01:16:10 +0000

    0x04. Debug #1.

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    newlist = my_list[:]
-    for i, val in enumerate(newlist):
+    # newlist = list(map(list, my_list))
+    newlist = []
+    for i, val in enumerate(my_list):
             if val == search:
-                newlist[i] = replace
+                newlist.append(replace)
+            else:
+                newlist.append(my_list[i])
+    return newlist
