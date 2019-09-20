5459a491fd9b84b12ec646d63f4502687c72acfb
jfkquality <jfkquality@gmail.com>
2019-09-20 03:01:42 +0000

    0x04. Debug #4. Used intersection().

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-def only_diff_elements(set_1, set_2):
-    set1list = list(set_1)
-    set2list = list(set_2)
-    bothsets = set1list + set2list
-    newset = []
-    for i, val1 in enumerate(bothsets):
-        if val1 not in newset:
-            newset.append(val1)
-    return set(newset)
+def only_diff_elements(set1, set2):
+    return set1.intersection(set2)
