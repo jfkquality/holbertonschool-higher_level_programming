a7997d6668d63eb2db6144a0612e1ad2053cb16e
jfkquality <jfkquality@gmail.com>
2019-09-30 20:53:18 +0000

    0x01 #13. Debug: insert 0 at top.

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *current = *head;
+	listint_t *new;
+
+        new = malloc(sizeof(listint_t));
+        if (new == NULL)
+        {
+                printf("Error\n");
+                return (NULL);
+        }
+        new->n = number;
+	new->next = NULL;
+	if (current == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	if ((*head)->n > number)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	while (current)
+	{
+		if (current->next != NULL)
+		{
+			if (current->next->n > number) /*insert new b4 next*/
+			{
+				new->next = current->next;
+				current->next = new;
+				return (new);
+			}
+		}
+		else
+		{
+			current->next = new;
+			return (new);
+		}
+		current = current->next;
+	}
+	return (NULL);
+}
