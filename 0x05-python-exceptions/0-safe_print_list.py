72a5366a29950893f5a631b62ff025a41d278fd4
jfkquality <jfkquality@gmail.com>
2019-09-30 23:59:07 +0000

    0x05 #0. Init commit.

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(mylist=[], x=0):
+        try:
+                for i in range(x):
+                        print(mylist[i], end='')
+                print()
+                return i + 1
+        except IndexError:
+                print()
+                return i
+        except:
+                print()
+                return i
