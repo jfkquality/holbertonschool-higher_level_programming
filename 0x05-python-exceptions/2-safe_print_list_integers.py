c04de66f6756cb642a8e0c431ea0905a99b4e72f
jfkquality <jfkquality@gmail.com>
2019-10-01 01:17:26 +0000

    0x05, #2. Debug for linter.

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,8 +9,5 @@ def safe_print_list_integers(mylist=[], x=0):
                         pass
                 except TypeError:
                         pass
-               # except:
-               #         print("list index out of RANGE")
-               #         pass
         print()
         return y
