0f758c912d8f522c7c34646ebae072db7aab7c98
jfkquality <jfkquality@gmail.com>
2019-10-01 00:30:29 +0000

    0x05. Debug #0. Use 'except: IndexError:' vs just 'except:'

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,8 @@ def safe_print_list(mylist=[], x=0):
         try:
                 for i in range(x):
                         print(mylist[i], end='')
-                print()
                 i += 1
-        except:
-                print()
+        except IndexError:
                 pass
+        print()
         return i
