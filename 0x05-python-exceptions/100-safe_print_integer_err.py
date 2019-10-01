620d03c2976f0acb90e692110943ee31842d3a53
jfkquality <jfkquality@gmail.com>
2019-10-01 17:05:17 +0000

    0x04. Debug #4, 7.

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 import sys
-def safe_print_integer(value):
-        except Exception as ex:
-                print("Exception: {}".format(ex), file=sys.stderr)
+
+
+def safe_print_integer_err(value):
+        try:
+                print("{:d}".format(value))
+        except Exception as e:
+                # print (e.message, e.args)
+                print("Exception:", e, file=sys.stderr)
+                return False
+        return True
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,15 +3,16 @@ def list_division(list1, list2, listlen):
         newlist = [None] * listlen
         try:
                 for i in range(listlen):
-                        newlist[i] = (list1[i]) / (list2[i])
-        except ZeroDivisionError:
-                newlist[i] = 0
-                print("division by 0")
-        except ValueError:
-                newlist[i] = 0
-                print("wrong type")
-        except TypeError:
-                newlist[i] = 0
-                print("wrong type")
-        # finally:
-        return newlist
+                        try:
+                                newlist[i] = (list1[i]) / (list2[i])
+                        except ZeroDivisionError:
+                                newlist[i] = 0
+                                print("division by 0")
+                        except TypeError:
+                                newlist[i] = 0
+                                print("wrong type")
+                        except IndexError:
+                                newlist[i] = 0
+                                print("out of range")
+        finally:
+                return newlist
