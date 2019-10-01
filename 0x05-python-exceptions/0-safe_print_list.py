685067b2522eef6e9d9030f64d2038a824fa0fd7
jfkquality <jfkquality@gmail.com>
2019-10-01 00:32:45 +0000

    0x05. Debug #0. Use separate variable for counter

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(mylist=[], x=0):
+        y = 0
         try:
                 for i in range(x):
                         print(mylist[i], end='')
-                i += 1
+                        y += 1
         except IndexError:
                 pass
         print()
-        return i
+        return y
