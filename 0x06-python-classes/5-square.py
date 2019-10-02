1985349b4bcef9c601fd50591c70a36a62b0664d
jfkquality <jfkquality@gmail.com>
2019-10-02 01:32:01 +0000

    0x06 #5. Init commit.

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+class Square:
+    """ A square object. """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = int(size)
+
+    def area(self):
+        """ Calculate area of the square"""
+        return (self.__size * self.__size)
+
+    def get_size(self):
+        """ Get private size variable """
+        return self.__size
+
+    def set_size(self, size):
+        """ Set private size variable """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = (size)
+
+    def my_print(self):
+        """ Print a square of size x size '#'s """
+        if self.__size == 0:
+            print()
+        else:
+            if not isinstance(self.__size, int):
+                raise TypeError("size must be an integer")
+            if (self.__size < 0):
+                raise ValueError("size must be >= 0")
+            for (x in range(self.__size)):
+                for (y in range(self.__size)):
+                    print("#", end='')
+                print()
+
+    size = property(get_size, set_size)
