2bb1a89015cb91aed51fa9660ce29686107b852f
jfkquality <jfkquality@gmail.com>
2019-10-03 01:37:36 +0000

    0x06 #5. Delete raise exception from init.

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -2,11 +2,11 @@
 class Square:
     """ A square object. """
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if (size < 0):
-            raise ValueError("size must be >= 0")
-        self.__size = int(size)
+        # if not isinstance(size, int):
+        #     raise TypeError("size must be an integer")
+        # if (size < 0):
+        #     raise ValueError("size must be >= 0")
+        self.size = int(size)
 
     def area(self):
         """ Calculate area of the square"""
