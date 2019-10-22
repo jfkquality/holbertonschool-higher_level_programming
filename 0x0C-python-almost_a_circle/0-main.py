7a7e91b399bfc28537c6831ff1ba9d76901a9a93
jfkquality <jfkquality@gmail.com>
2019-10-22 20:43:23 +0000

    0x0C #1. Remove debugging code

--- 0x0C-python-almost_a_circle/0-main.py
+++ 0x0C-python-almost_a_circle/0-main.py
@@ -4,22 +4,17 @@ from models.base import Base
 
 if __name__ == "__main__":
 
-    b1 = Base('abc')
-    # print(type(b1.id))
+    b1 = Base()
     print(b1.id)
 
     b2 = Base()
-    # print(type(b2.id))
     print(b2.id)
 
     b3 = Base()
-    # print(type(b3.id))
     print(b3.id)
 
     b4 = Base(12)
-    # print(type(b4.id))
     print(b4.id)
 
     b5 = Base()
-    # print(type(b5.id))
     print(b5.id)
