5715ba6f6bc2e65048384dceeba7fa504576c1ca
jfkquality <jfkquality@gmail.com>
2019-10-23 04:38:37 +0000

    0x0C. Correct output, args, xargs rectangle tests.

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -165,89 +165,96 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(output, '\n\n  ##\n  ##\n  ##\n')
 
 
+    def test_args_print(self):
+        """ Test if args update attributes, prints
+        """
+        from io import StringIO
+        import io
+        import contextlib
+
         r1 = Rectangle(10, 10, 10, 10)
         temp_stdout = io.StringIO()
         with contextlib.redirect_stdout(temp_stdout):
             print(r1)
         output = temp_stdout.getvalue()
-        self.assertEqual(output, '[Rectangle] (1) 10/10 - 10/10')
+        self.assertEqual(output, '[Rectangle] (1) 10/10 - 10/10\n')
 
         r1.update(89)
         temp_stdout = io.StringIO()
         with contextlib.redirect_stdout(temp_stdout):
             print(r1)
         output = temp_stdout.getvalue()
-        self.assertEqual(output, '[Rectangle] (89) 10/10 - 10/10')
+        self.assertEqual(output, '[Rectangle] (89) 10/10 - 10/10\n')
 
         r1.update(89, 2)
         temp_stdout = io.StringIO()
         with contextlib.redirect_stdout(temp_stdout):
             print(r1)
         output = temp_stdout.getvalue()
-        self.assertEqual(output, '[Rectangle] (89) 10/10 - 2/10')
+        self.assertEqual(output, '[Rectangle] (89) 10/10 - 2/10\n')
 
         r1.update(89, 2, 3)
         temp_stdout = io.StringIO()
         with contextlib.redirect_stdout(temp_stdout):
             print(r1)
         output = temp_stdout.getvalue()
-        self.assertEqual(output, '[Rectangle] (89) 10/10 - 2/3')
+        self.assertEqual(output, '[Rectangle] (89) 10/10 - 2/3\n')
 
         r1.update(89, 2, 3, 4)
         temp_stdout = io.StringIO()
         with contextlib.redirect_stdout(temp_stdout):
             print(r1)
         output = temp_stdout.getvalue()
-        self.assertEqual(output, '[Rectangle] (89) 4/10 - 2/3')
+        self.assertEqual(output, '[Rectangle] (89) 4/10 - 2/3\n')
 
         r1.update(89, 2, 3, 4, 5)
         temp_stdout = io.StringIO()
         with contextlib.redirect_stdout(temp_stdout):
             print(r1)
         output = temp_stdout.getvalue()
-        self.assertEqual(output, '[Rectangle] (89) 4/5 - 2/3')
+        self.assertEqual(output, '[Rectangle] (89) 4/5 - 2/3\n')
+
+    def test_xargs_print(self):
+        """ Test if xargs updates, prints
+        """
+        from io import StringIO
+        import io
+        import contextlib
 
         r1 = Rectangle(10, 10, 10, 10)
         temp_stdout = io.StringIO()
         with contextlib.redirect_stdout(temp_stdout):
             print(r1)
         output = temp_stdout.getvalue()
-        self.assertEqual(output, '[Rectangle] (1) 10/10 - 10/10')
+        self.assertEqual(output, '[Rectangle] (1) 10/10 - 10/10\n')
 
         r1.update(height=1)
         temp_stdout = io.StringIO()
         with contextlib.redirect_stdout(temp_stdout):
             print(r1)
         output = temp_stdout.getvalue()
-        self.assertEqual(output, '[Rectangle] (1) 10/10 - 10/1')
+        self.assertEqual(output, '[Rectangle] (1) 10/10 - 10/1\n')
 
         r1.update(width=1, x=2)
         temp_stdout = io.StringIO()
         with contextlib.redirect_stdout(temp_stdout):
             print(r1)
         output = temp_stdout.getvalue()
-        self.assertEqual(output, '[Rectangle] (1) 2/10 - 1/1')
+        self.assertEqual(output, '[Rectangle] (1) 2/10 - 1/1\n')
 
         r1.update(y=1, width=2, x=3, id=89)
         temp_stdout = io.StringIO()
         with contextlib.redirect_stdout(temp_stdout):
             print(r1)
         output = temp_stdout.getvalue()
-        self.assertEqual(output, '[Rectangle] (89) 3/1 - 2/1')
+        self.assertEqual(output, '[Rectangle] (89) 3/1 - 2/1\n')
 
         r1.update(x=1, height=2, y=3, width=4)
         temp_stdout = io.StringIO()
         with contextlib.redirect_stdout(temp_stdout):
             print(r1)
         output = temp_stdout.getvalue()
-        self.assertEqual(output, '[Rectangle] (89) 3/1 - 2/1')
-
-        r1.update(x=1, height=2, y=3, width=4)
-        temp_stdout = io.StringIO()
-        with contextlib.redirect_stdout(temp_stdout):
-            print(r1)
-        output = temp_stdout.getvalue()
-        self.assertEqual(output, '[Rectangle] (89) 1/3 - 4/2')
+        self.assertEqual(output, '[Rectangle] (89) 1/3 - 4/2\n')
 
 
     if __name__ == '__main__':
