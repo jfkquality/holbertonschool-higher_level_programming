1618649f653af7211c350045869599eaf08cacc7
jfkquality <jfkquality@gmail.com>
2019-11-26 02:59:36 +0000

    0x0D SQL Tasks 4, init commit.

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,2 @@
 -- Create table in database
-CREATE DATABASE IF NOT EXISTS
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256))
