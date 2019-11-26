605805cd6cd924105b5d826dbb8a6889c67c7767
jfkquality <jfkquality@gmail.com>
2019-11-26 04:56:03 +0000

    0x0D SQL Task 13. Sort by score descending

--- 0x0D-SQL_introduction/9-full_creation2.sql
+++ 0x0D-SQL_introduction/9-full_creation2.sql
@@ -3,11 +3,7 @@ CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 
 INSERT INTO second_table (id, name, score)
 VALUES
-	(5, "Jim", 10),
-	(6, "Alex", 3),
-	(7, "Bob", 14),
-	(8, "Jim", 10),
-	(9, "", 3),
-	(10, "Sam", 14),
-	(11, "", 8),
-	(12, "Mary", 8);
+	(5, "Joe", 5),
+	(6, "Pam", 3),
+	(7, "Lauren", 4),
+	(12, "Alexis", 5);
