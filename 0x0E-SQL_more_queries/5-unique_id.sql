254d48291a452d8125dbf2f9ed683f9fb3c759ed
jfkquality <jfkquality@gmail.com>
2019-11-27 00:57:54 +0000

    0x0E SQL 102. Task 5. Init commit.

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,10 +1,10 @@
--- creates the table id_not_null on your MySQL server.
+-- creates the table unique_id on your MySQL server.
 -- unique_id description:
--- id INT DEFAULT 1
+-- id INT DEFAULT 1 UNIQUE
 -- name VARCHAR(256)
 -- The database name will be passed as an argument of the mysql command
 -- If the table force_name already exists, your script should not fail
 CREATE TABLE IF NOT EXISTS id_not_null (
-       id INT DEFAULT 1,
+       id INT DEFAULT 1 UNIQUE,
        name VARCHAR(256)
 );
