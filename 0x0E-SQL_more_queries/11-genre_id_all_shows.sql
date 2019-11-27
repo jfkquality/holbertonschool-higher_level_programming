b2aaa45a04d85f88b89eea5c134375751d43602d
jfkquality <jfkquality@gmail.com>
2019-11-27 05:12:46 +0000

    0x0E SQL 102. Task 11. Init commit.

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,9 @@
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- You can use only one SELECT statement
+SELECT tv.title, tvsg.genre_id
+FROM tv_shows AS tv
+INNER JOIN tv_show_genres AS tvsg
+ON tv.id = tvsg.show_id
+ORDER BY tv.title ASC, tvsg.genre_id ASC
