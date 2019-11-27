5803de1a219c33a406e7690a46fdf0667993df9a
jfkquality <jfkquality@gmail.com>
2019-11-27 00:59:56 +0000

    0x0E SQL 102. Task 5. Correct wrong table name.

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -4,7 +4,7 @@
 -- name VARCHAR(256)
 -- The database name will be passed as an argument of the mysql command
 -- If the table force_name already exists, your script should not fail
-CREATE TABLE IF NOT EXISTS id_not_null (
+CREATE TABLE IF NOT EXISTS unique_id (
        id INT DEFAULT 1 UNIQUE,
        name VARCHAR(256)
 );
