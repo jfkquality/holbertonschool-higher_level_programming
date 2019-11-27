91355880ed1256d932147c85ac5e442751b7ace4
jfkquality <jfkquality@gmail.com>
2019-11-27 04:15:32 +0000

    0x0E SQL 102. Task 8. Init commit.

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,12 @@
+-- lists all the cities of California that can be found in the database hbtn_0d_usa.
+-- The states table contains only one record where name = California
+-- (but the id can be different, as per the example)
+-- Results must be sorted in ascending order by cities.id
+-- You are not allowed to use the JOIN keyword
+-- The database name will be passed as an argument of the mysql command
+SELECT c.id, c.name
+FROM cities AS c
+INNER JOIN states AS s
+ON c.state_id = s.id
+WHERE s.name = CALIFORNIA
+ORDER BY c.id ASC
