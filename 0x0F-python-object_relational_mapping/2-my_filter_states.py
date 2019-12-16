8bbf6fb60899bef9c1b51b31959c43b2c535f825
jfkquality <jfkquality@gmail.com>
2019-12-16 02:51:10 +0000

    0x0F, Task 2. Init commit.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" List all states in database table """
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    myuser = argv[1]
+    mypwd = argv[2]
+    mydb = argv[3]
+    statesearch = argv[4]
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=myuser,
+                         passwd=mypwd,
+                         db=mydb)
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    for id, name in cur.fetchall():
+        if name == statesearch:
+            print("({}, '{}')".format(id, name))
+
+    cur.close()
+    db.close()
