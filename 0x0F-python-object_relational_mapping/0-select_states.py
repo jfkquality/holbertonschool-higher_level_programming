b20c6d12a431b3af81b23280cdff243edf121f6f
jfkquality <jfkquality@gmail.com>
2019-12-16 01:06:50 +0000

    0x0F, Task 0. Init commit.

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    myuser = argv[1]
+    mypwd = argv[2]
+    mydb = argv[3]
+
+    db = MySQLdb.connect(host="localhost",
+                         user=myuser,
+                         passwd=mypwd,
+                         db=mydb)
+
+    cur=db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id")
+    for id, name in cur.fetchall():
+        print ("({}, '{}')".format(id, name))
+
+    db.close
