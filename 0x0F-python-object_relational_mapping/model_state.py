c3c891ff81931db672af4b971b60f158a3d35972
jfkquality <jfkquality@gmail.com>
2019-12-17 20:52:01 +0000

    0x0F, ORM, SQLAlchemy. Task 6. Deleting engine block worked. Put back import sqlalchemy.

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,6 +2,7 @@
 """ Base Class
 """
 
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
