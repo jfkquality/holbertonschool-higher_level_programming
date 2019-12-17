1b39e2ddb145cbfc0049ac27cd0afb05f1ee2ab6
jfkquality <jfkquality@gmail.com>
2019-12-17 21:32:27 +0000

    0x0F, ORM, SQLAlchemy. Task 6. Clean up commented lines.

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,14 +6,8 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-# """ Import sqlalchemy module """
 
 Base = declarative_base()
-# """ Base module """
-# connect to server
-# engine = create_engine('mysql://root:jknight121@localhost:3306')
-# Base.metadata.create_all(engine)
-
 
 class State(Base):
     """State subclass of Base"""
