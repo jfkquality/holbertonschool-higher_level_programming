274a77feffffae494354f1c09b3aca3276b2efc8
jfkquality <jfkquality@gmail.com>
2019-12-17 20:48:05 +0000

    0x0F, ORM, SQLAlchemy. Task 6. Remove import sqlalchemy.

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,7 +2,6 @@
 """ Base Class
 """
 
-import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
