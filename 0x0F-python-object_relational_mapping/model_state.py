366981154e156b6519fbeae712453ac271c2b4ec
jfkquality <jfkquality@gmail.com>
2019-12-17 21:01:44 +0000

    0x0F, ORM, SQLAlchemy. Task 6. __tablename__ line needed.

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -17,7 +17,7 @@ Base = declarative_base()
 
 class State(Base):
     """State subclass of Base"""
-    # __tablename__ = 'states'
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, unique=True)
     # id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
