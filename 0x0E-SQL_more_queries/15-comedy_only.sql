2f5d73306d3f66df24224d7e160d2e401cb95e92
chriswill88 <williechri79@gmail.com>
2019-12-29 01:41:59 +0000

    0x0E, Task 15. Init commit.

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,14 @@
+-- lists all Comedy shows in the database hbtn_0d_tvshows.
+-- The tv_genres table contains only one record where name = Comedy (but the id can be different)
+-- Each record should display: tv_shows.title
+-- Results must be sorted in ascending order by the show title
+-- You can use only one SELECT statement
+
+SELECT s.title
+FROM tv_shows as s
+INNER JOIN tv_show_genres AS tvsg
+ON s.id = tvsg.show_id
+INNER JOIN tv_genres AS g
+ON g.id = tvsg.genre_id
+WHERE g.name = "Comedy"
+ORDER BY s.title ASC
