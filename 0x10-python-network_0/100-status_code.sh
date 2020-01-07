c76b3e312a6bf43c4ed641c90cd7a14fb1c68b1e
jfkquality <jfkquality@gmail.com>
2020-01-07 07:34:41 +0000

    0x10. Task 7. Use no pipes or redirection.

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # send request to URL and displays only the status code of the response.
-curl -sIL "$1" | grep -Fi "200 OK" | cut -d ' ' -f2
+# curl -sIL "$1" | grep -Fi "200 OK" | cut -d ' ' -f2
+curl -sI -o /dev/null -w "%{http_code}" "$1"
