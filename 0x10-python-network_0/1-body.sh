fd905e0e7985b03f404c934aaa91d63dd6418fc8
jfkquality <jfkquality@gmail.com>
2020-01-07 01:19:52 +0000

    0x10. Task 1. Forgot to put -L!

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # take a URL, send GET request, display only body of a 200 status code response
-curl -sI "$1" | grep "HTTP/1.0 200" | cut -d ' ' -f3-
+curl -sIL "$1" | grep "HTTP/1.0 200" | cut -d ' ' -f3-
