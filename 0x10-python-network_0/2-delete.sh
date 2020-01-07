3214478f3a258c39f1521dce746c3802090a673b
jfkquality <jfkquality@gmail.com>
2020-01-07 05:17:51 +0000

    0x10. Task 2. Add back -d <body> text.

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # take a URL, send GET request, display only body of a 200 status code response
 # send DELETE request to the URL passed and displays the body of the response
-curl -s -X "DELETE" "$1"
+curl -s -X "DELETE" -d "I\'m a DELETE request" "$1"
