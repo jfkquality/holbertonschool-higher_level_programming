6a7cec1a597e989290544eba79ea7829d12916a1
jfkquality <jfkquality@gmail.com>
2020-01-07 03:46:58 +0000

    0x10. Task 1. Remove cut; show entire status line.

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # take a URL, send GET request, display only body of a 200 status code response
-curl  "$1" -sLI | grep "200 OK" | cut -d ' ' -f2- | sed -e "s/[^a-zA-Z0-9 ]//g"
+curl  "$1" -sLI | grep "200 OK" | sed -e "s/[^a-zA-Z0-9.// ]//g"  #| cut -d ' ' -f2-
