95201d234b737f24546bc1a0f66bdbc993c38596
jfkquality <jfkquality@gmail.com>
2020-01-07 05:50:36 +0000

    0x10. Task 4. Init commit.

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL and displays all HTTP methods the server will accept.
-curl -sI "$1" | grep -Fi Allow | cut -d ' ' -f2-  # | sed -e "s/[^0-9]//g"
+curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
