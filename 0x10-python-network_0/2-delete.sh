77a91038e6d32db4d522559348fd397015ecdc22
jfkquality <jfkquality@gmail.com>
2020-01-07 04:21:51 +0000

    0x10. Task 2. 2nd try.

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # take a URL, send GET request, display only body of a 200 status code response
 # send DELETE request to the URL passed and displays the body of the response
-curl "$1" -s -X DELETE
+curl -s -X DELETE "$1"
