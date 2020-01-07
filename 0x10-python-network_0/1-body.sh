1105839332d678758d7c74546e9d900be9d93df9
jfkquality <jfkquality@gmail.com>
2020-01-07 03:39:51 +0000

    0x10. Task 1. Delete extra commented line.

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # take a URL, send GET request, display only body of a 200 status code response
-# curl  "$1" -sLI | grep "200 OK" | cut -d ' ' -f3-
 curl  "$1" -sLI | grep "200 OK" | cut -d ' ' -f3- | sed -e "s/[^a-zA-Z0-9 ]//g"
