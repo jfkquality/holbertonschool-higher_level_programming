540ea985ae0495094071d25d88a0bbf0d16233d6
jfkquality <jfkquality@gmail.com>
2020-01-07 02:52:24 +0000

    0x10. Task 1. Include f2 in output.

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # take a URL, send GET request, display only body of a 200 status code response
-curl -sIL "$1" | grep "200 OK"  # | cut -d ' ' -f 3-
+curl  "$1" -sLI | grep "200 OK" | cut -d ' ' -f2-
