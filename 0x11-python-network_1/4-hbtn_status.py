ab23bf761413896fda6e827e1bd7a191e132c7ab
jfkquality <jfkquality@gmail.com>
2020-01-14 04:14:05 +0000

    0x11. Task 4. Init commit.

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 # fetch https://intranet.hbtn.io/status. You must use the package requests
+import requests
 
+r = requests.get('https://intranet.hbtn.io/status')
+print("Body response:")
+print("\t- type: {}".format(type(r.text)))
+print("\t- content: {}".format(r.text))
