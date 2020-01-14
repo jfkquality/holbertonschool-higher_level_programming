7e7e3f0d269b4a22f26f64b2c9f2d59c073e0cdd
jfkquality <jfkquality@gmail.com>
2020-01-14 04:32:24 +0000

    0x11. Task 6. Init commit.

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# sends a request to URL argv, display X-Request-Id in the response header
+import requests
+from sys import argv
+
+url = argv[1]
+email = argv[2]
+r = requests.post(url, email)
+print(r.text)
