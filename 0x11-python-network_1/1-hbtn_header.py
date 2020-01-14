dfb9d4227009ebaf3fdf2587e607362d7bc75b72
jfkquality <jfkquality@gmail.com>
2020-01-14 01:47:28 +0000

    0x11. Task 1. Forgot argv.

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 # send request to URL and display value of X-Request-Id variable in the headef
 
-import urllib.request
-import sys
+from urllib import request
+from sys import argv
 
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
+    url = argv[1]
+    with request.urlopen(url) as response:
         headers = response.info().items()
     for line in headers:
         if line[0] == "X-Request-Id":
