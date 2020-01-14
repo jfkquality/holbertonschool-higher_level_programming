915138bb073da2035a6940580ae702c559de8fd1
jfkquality <jfkquality@gmail.com>
2020-01-14 04:05:28 +0000

    0x11. Task 2. Linter err: line too long.

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# take in URL and email, send a POST req with email as param, display body of the response (decoded in utf-8)
+# take in URL and email, send a POST req with email as param, display resp/utf8
 from urllib import request
 from urllib import parse
 from sys import argv
@@ -7,7 +7,7 @@ from sys import argv
 if __name__ == "__main__":
     url = argv[1]
     email = argv[2]
-    values = {'mailto' : email}
+    values = {'mailto':email}
     data = parse.urlencode(values)
     data = data.encode('ascii')
     req = request.Request(url, data)
