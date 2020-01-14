329d7246722c82b2b1888494628cb5707e924b51
jfkquality <jfkquality@gmail.com>
2020-01-14 03:58:28 +0000

    0x11. Task 3. Init commit.

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# take in URL and email, send a POST req with email as param, display body of the response (decoded in utf-8)
+from urllib import request
+from urllib import parse
+from urllib import error
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    req = request.Request(url)
+    try:
+        with request.urlopen(req) as response:
+            headers = response.info().items()
+            html = response.read()
+            utf8 = html.decode('UTF-8')
+            print(utf8)
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
