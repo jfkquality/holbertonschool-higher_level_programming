8f836a65047ef433b1eac55604df7a2a5dee20ce
jfkquality <jfkquality@gmail.com>
2020-01-14 04:03:19 +0000

    0x11. Task 2. Init commit.

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# take in URL and email, send a POST req with email as param, display body of the response (decoded in utf-8)
+from urllib import request
+from urllib import parse
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    email = argv[2]
+    values = {'mailto' : email}
+    data = parse.urlencode(values)
+    data = data.encode('ascii')
+    req = request.Request(url, data)
+    with request.urlopen(req) as response:
+        headers = response.info().items()
+        html = response.read()
+        utf8 = html.decode('UTF-8')
+    print(utf8)
---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+# fetch https://intranet.hbtn.io/status. You must use the package requests
+
