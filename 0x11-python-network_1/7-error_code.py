5df9a9ccc668cccbfafc8b0842b804eb7f36fc70
jfkquality <jfkquality@gmail.com>
2020-01-14 05:09:22 +0000

    0x11. Task 7. Init commit.

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# sends a request to URL argv, display X-Request-Id in the response header
+import requests
+from sys import argv
+
+url = argv[1]
+r = requests.get(url)
+if r.status_code > 400:
+    print("Error code: {}".format(r.status_code))
+else:
+    print(r.text)
