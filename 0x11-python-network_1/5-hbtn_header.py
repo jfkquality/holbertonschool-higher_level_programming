46d9b20d2e41c7269cffbf3ff239498f9f4ff13f
jfkquality <jfkquality@gmail.com>
2020-01-14 04:24:23 +0000

    0x11. Task 5. Had extra comment line!

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# fetch https://intranet.hbtn.io/status. You must use the package requests
 # sends a request to URL argv, display X-Request-Id in the response header
 import requests
 from sys import argv
