e0fd5459263d09045e073c2fc428c22e535f18cb
jfkquality <jfkquality@gmail.com>
2020-01-14 05:15:01 +0000

    0x11. Task 7. Add = to > 400.

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,7 +5,7 @@ from sys import argv
 
 url = argv[1]
 r = requests.get(url)
-if r.status_code > 400:
+if r.status_code >= 400:
     print("Error code: {}".format(r.status_code))
 else:
     print(r.text)
