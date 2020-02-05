08fc7ea5a70e05c0c427ad7829ffe9f03d0ed01e
jfkquality <jfkquality@gmail.com>
2020-02-05 03:45:24 +0000

    Task 5. Use pareInt() vs Number.isNumber().

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-if Number.isInteger(process.argv[2]) {
-  console.log('My number: ' + process.argv[2]);
+// if (Number.isInteger(process.argv[2])) {
+if (parseInt(process.argv[2])) {
+  console.log('My number: ' + parseInt(process.argv[2]));
 } else {
   console.log('Not a number');
 }
