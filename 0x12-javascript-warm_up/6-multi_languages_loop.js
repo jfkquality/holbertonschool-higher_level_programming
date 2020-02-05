42551fa511a0f2e9f33e37cb1fdabe2fea5582ab
jfkquality <jfkquality@gmail.com>
2020-02-05 04:03:27 +0000

    Task 6. Refactor using for-in loop vs forEach().

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-languages.forEach(line => {
-  console.log(line);
-});
+for (const line in languages) {
+  console.log(languages[line]);
+}
+// languages.forEach(line => {
+//   console.log(line);
+// });
