cf18a78a9e8f3059127e5df92fec93652f4e5aa0
jfkquality <jfkquality@gmail.com>
2020-02-05 04:20:14 +0000

    Task 8. Use const for non-changing variables.

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,8 +2,8 @@
 // if (Number.isInteger(process.argv[2])) {
 if (parseInt(process.argv[2])) {
   let i = process.argv[2];
-  let j = i;
-  let x = 'x';
+  const j = i;
+  const x = 'x';
   while (i > 0) {
     console.log(x.repeat(j));
     i--;
