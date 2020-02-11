f320992413f47a3ce4ba342b984dca0f2120c820
jfkquality <jfkquality@gmail.com>
2020-02-11 03:08:26 +0000

    Task 0. Where in any of the resources was module.exports???.

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,7 +1,2 @@
 #!/usr/bin/node
-class Rectangle { // eslint-disable-line no-unused-vars
-  constructor (h, w) {
-    this.height = h;
-    this.width = w;
-  }
-}
+module.exports = class Rectangle {};
