0b96f9978020e82c9fce0bda30e1d58cf2b39cf5
jfkquality <jfkquality@gmail.com>
2020-02-11 05:34:51 +0000

    Task  7. Linter errors.

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,9 +2,9 @@
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
   for (let i = 0; i < list.length; i++) {
-    if (list[i] == searchElement) {
+    if (list[i] === searchElement) {
       count++;
     }
   }
   return (count);
-}
+};
