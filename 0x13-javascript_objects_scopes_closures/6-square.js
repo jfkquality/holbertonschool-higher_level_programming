bee40a58f29292393ec3772c51936bc9f8cb76d9
jfkquality <jfkquality@gmail.com>
2020-02-11 05:09:05 +0000

    Task 6: Init commit.

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,43 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+  charPrint(c) {
+    this.chr = c;
+    if (!this.chr) {
+      this.chr = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      console.log(this.chr.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
