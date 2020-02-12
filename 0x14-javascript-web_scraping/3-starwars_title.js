e7a91e875819a04d531968349c068b5bf519b462
jfkquality <jfkquality@gmail.com>
2020-02-12 05:16:18 +0000

    Task 4: Init commit.

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,6 +4,7 @@ const url = 'http://swapi.co/api/films/' + process.argv[2];
 
 request(url, function (error, response, body) {
   if (error) throw (error);
-  const json = JSON.parse(body);
-  console.log(json.title);
+  const film = JSON.parse(body);
+  console.log(film.title);
+  console.log(film.characters)
 });
