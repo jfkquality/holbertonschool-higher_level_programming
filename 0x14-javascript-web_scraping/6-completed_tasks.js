781a3715328fc9b5ade7abd2ab34674a44f9a66d
jfkquality <jfkquality@gmail.com>
2020-02-13 06:58:58 +0000

    Task 6. Init commit.

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,15 +6,15 @@ request(url, function (error, response, body) {
   if (error) throw (error);
   const todos = JSON.parse(body);
 
-  let results = {};
+  const results = {};
 
   todos.forEach(function (todo, index, arr) {
-    if (!results[todo.userId]) {
-      console.log("First userId: " + todo.userId);
-      results[todo.userId] = 1;
-    } else {
-      console.log('Increment userId: ' + todo.userId + ' count ' + results[todo.userId]);
-      results[todo.userId] += 1;
+    if (todo.completed) {
+      if (!results[todo.userId]) {
+        results[todo.userId] = 1;
+      } else {
+        results[todo.userId] += 1;
+      }
     }
   });
   console.log(results);
