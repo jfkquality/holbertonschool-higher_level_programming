38b9be710980d429e6f6c531bd4e286b640142e7
jfkquality <jfkquality@gmail.com>
2020-06-17 22:51:47 -0400

    Task 10, Refactor. Use a different algorithm.

--- 0x00-python-hello_world/10-check_cycle-oldway.c
+++ 0x00-python-hello_world/10-check_cycle-oldway.c
@@ -7,21 +7,11 @@ int check_cycle(listint_t *list)
 {
 	listint_t *head = list;
 	listint_t *current = head;
-	listint_t *slow = head, *fast = head;
-
 	while (current)
 	{
-		slow = slow->next;
-
-		if (fast->next != NULL)
-			fast = fast->next->next;
-		else
-			return (0);
-
-		if (slow == NULL || fast == NULL)
-			return (0);
-		if (slow == fast)
+		if (current->next == head)
 			return (1);
+		current = current->next;
 	}
 	return (0);
 }
